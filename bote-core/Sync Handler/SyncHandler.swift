cc327621c0acc39747cdfaa6cb425f2cce70a5a6
Pascal Braband <pascal.braband@web.de>
2019-12-11 18:40:19 +0100

    Added SyncHandler protocol

---
+++ bote-core/Sync Handler/SyncHandler.swift
@@ -0,0 +1,22 @@
+//
+//  SyncHandler.swift
+//  bote-core
+//
+//  Created by Pascal Braband on 11.12.19.
+//  Copyright © 2019 Pascal Braband. All rights reserved.
+//
+// Interesting library: https://github.com/amosavian/FileProvider (WebDAV, FTP, Dropbox, OneDrive)
+
+import Foundation
+
+protocol SyncHandler {
+    init(configuration: ConnectionConfiguration)
+    func uploadFile(path: String)
+    func removeFile(path: String)
+    func renameFile(src: String, dst: String)
+    
+    // SyncHandlers should connect automatically, just a convenience function
+    func connect()
+    func terminate()
+}
+
