47be68f4c47aa36d41a6fc1ea102f8b2c30246a9
Pascal Braband <pascal.braband@web.de>
2019-12-11 18:11:04 +0100

    LocalFileWatcherTests: 	- Moved file related methods into separate file 	- cleaned (removed prints and unnecessary comments)

--- bote-core-tests/LocalFileWatcherTests.swift
+++ bote-core-tests/LocalFileWatcherTests.swift
@@ -11,8 +11,7 @@ import Combine
 
 class LocalFileWatcherTests: XCTestCase {
     
-    let watchPath = "/private/tmp/bote-core-tests"
-    //let watchPath = "/Users/pascal/Desktop/watchDir"
+    let watchPath = testsBasepath
     var subscriber: AnyCancellable?
 
     override func setUp() {
@@ -33,7 +32,6 @@ class LocalFileWatcherTests: XCTestCase {
         subscriber = LocalFileWatcher.init(watchPath: watchPath).sink(receiveCompletion: { (completion) in
             self.checkComplition(completion: completion)
         }) { (event: FileEvent) in
-            print("##### EVENT: \(event)")
             if case .createdFile(path: let path) = event {
                 if path == filepath {
                     expectation.fulfill()
@@ -56,7 +54,6 @@ class LocalFileWatcherTests: XCTestCase {
         subscriber = LocalFileWatcher.init(watchPath: watchPath).sink(receiveCompletion: { (completion) in
             self.checkComplition(completion: completion)
         }) { (event: FileEvent) in
-            print("RRRR EVENT: \(event)")
             if case .removedFile(path: let path) = event {
                 if path == filepath {
                     expectation.fulfill()
@@ -198,7 +195,8 @@ class LocalFileWatcherTests: XCTestCase {
         }
         
         moveItem(from: srcFilepath, to: dstFilepath)
-        print("EVENTS: \(registeredEvents)")
+        
+        // Check if all events were registered
         DispatchQueue.global().asyncAfter(deadline: .now() + 0.5) {
             if registeredEvents.contains(FileEvent.createdDir(path: dstFilepath))
                 && registeredEvents.contains(FileEvent.createdFile(path: dstFilepath + fileA))
@@ -278,84 +276,4 @@ class LocalFileWatcherTests: XCTestCase {
             XCTFail("Publisher unexpectedly failed. \(error)")
         }
     }
-    
-    
-    
-    
-    // MARK: - File Managment
-    
-    func createFile(at path: String) {
-        safetyNet(path: path) {
-            shell("touch \(path)")
-        }
-    }
-    
-    
-    func removeFile(at path: String) {
-        safetyNet(path: path) {
-            shell("rm \(path)")
-        }
-    }
-    
-    
-    func createDir(at path: String) {
-        safetyNet(path: path) {
-            shell("mkdir \(path)")
-        }
-    }
-    
-    
-    func removeDir(at path: String) {
-        safetyNet(path: path) {
-            shell("rm -rf \(path)")
-        }
-        
-    }
-    
-    
-    func moveItem(from src: String, to dst: String) {
-        safetyNet(path: src, dst) {
-            shell("mv \(src) \(dst)")
-        }
-    }
-    
-    
-    @discardableResult
-    func shell(_ command: String) -> String {
-        print("###CMD \(command)")
-        let task = Process()
-        task.launchPath = "/bin/zsh"
-        task.arguments = ["-c", command]
-
-        let pipe = Pipe()
-        task.standardOutput = pipe
-        task.launch()
-
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output: String = NSString(data: data, encoding: String.Encoding.utf8.rawValue)! as String
-
-        return output
-    }
-    
-    
-    func safetyNet(path: String..., completion: () -> ()) {
-        // Should prevent accidently operating on wrong path in file system
-        if path.allSatisfy({ $0.hasPrefix(watchPath) }) {
-            completion()
-        } else {
-            fatalError("Operating on unauthorized path.")
-        }
-    }
-    
-    
-    /*func SafetyNet() {
-        let allowedA = watchPath + "/testA"
-        let allowedB = watchPath + "/testB"
-        let forbiddenA = "/Users/pascal/"
-        let forbiddenB = "/Users/forbidden"
-        
-        safetyNet(path: allowedA, allowedB, forbiddenB) {
-            // Should produce fatal error
-        }
-    }*/
 }
