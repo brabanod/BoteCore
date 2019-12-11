f39a46216286e1cdc498995be46df5d3a0fa6780
Pascal Braband <pascal.braband@web.de>
2019-12-11 18:55:37 +0100

    Updated SFTPConfiguration. Can now decide which authentication method should be used Added Shout SSH/SFTP framework.

--- bote-core/Configuration/SFTPConfiguration.swift
+++ bote-core/Configuration/SFTPConfiguration.swift
@@ -8,12 +8,17 @@
 
 import Foundation
 
+enum SFTPAuthentication {
+    case password(keychainId: Int)
+    case key(path: String)
+}
+
 struct SFTPConfiguration: ConnectionConfiguration {
     var path: String
     var host: String
     var port: Int
-    var user: String
-    var password: String
+    var authentication: SFTPAuthentication
 }
 
 // FIXME: Save username and password in keychain and save keychain id in SFTPConfiguration
+// TODO: Either supply user/password or path to SSH Key
