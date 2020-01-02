e0c0c77064a4f82d55c9c8bacf6c836cdc7da03d
Pascal Braband <pascal.braband@web.de>
2020-01-02 19:14:01 +0100

    Added SafetyNet class to use when operating on the file system.

---
+++ bote-core/Helpers/SafetyNet.swift
@@ -0,0 +1,42 @@
+//
+//  SafetyNet.swift
+//  bote-core
+//
+//  Created by Pascal Braband on 02.01.20.
+//  Copyright © 2020 Pascal Braband. All rights reserved.
+//
+
+import Foundation
+
+enum SafetyNetError: Error {
+    case unauthorized(String)
+}
+
+class SafetyNet {
+    
+    private let basePath: String
+    
+    
+    init(basePath: String) {
+        self.basePath = basePath
+    }
+    
+    
+    /**
+     Checks if the given paths are inside the basepath, with which the object was initialized.
+     
+     - parameters:
+        - path: A list of paths as `String`'s. These are the paths, that `SafetyNet` compares to the basepath.
+    
+     - throws:
+        - `SafetyNetError`: If one of the given paths is outside of the basepath, an error is thrown.
+     */
+    func intercept(path: String...) throws {
+        // Should prevent accidently operating on wrong path in file system
+        if path.allSatisfy({ $0.hasPrefix(testsBasepath) }) {
+            return
+        } else {
+            throw SafetyNetError.unauthorized("Operating on unauthorized path: \(path).\n Only operations on \(basePath) are allowed.")
+        }
+    }
+}
