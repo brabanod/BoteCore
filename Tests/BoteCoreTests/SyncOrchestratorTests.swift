63f377245717c987df8172c3347d7738f69bfc75
Pascal Braband <pascal.braband@web.de>
2020-02-25 23:09:32 +0100

    SyncOrchestratorTests: Fixed wrong commented code

--- Tests/BoteCoreTests/SyncOrchestratorTests.swift
+++ Tests/BoteCoreTests/SyncOrchestratorTests.swift
@@ -12,43 +12,43 @@ class SyncOrchestratorTests: XCTestCase {
     
     var defaultTransferHandler: SFTPTransferHandler?
     
-//    override func setUp() {
-//        // Create local test directory
-//        createDir(at: testsBasepath)
-//
-//        // Create remote test directory
-//        do {
-//            let f = LocalConnection(path: testsBasepath)
-//            let t = try SFTPConnection(path: SFTPServer.path, host: SFTPServer.host, port: SFTPServer.port, user: SFTPServer.user, authentication: .password(value: SFTPServer.password))
-//            defaultTransferHandler = SFTPTransferHandler.init(from: f, to: t)
-//            try defaultTransferHandler?.upload(path: testsBasepath, isDir: true)
-//        } catch let error {
-//            if error is KeychainError {
-//                fatalError("Couldn't create SFTPConnection object.\nERROR: \(error)")
-//            } else {
-//                fatalError("Couldn't upload remote test directory at path \'\(SFTPServer.path)\'.\nERROR: \(error)")
-//            }
-//        }
-//    }
-//
-//    override func tearDown() {
-//        // Remove keychain item
-//        do {
-//            try KeychainGuard.removeItem(user: SFTPServer.user, server: SFTPServer.host)
-//        } catch _ {
-//            print("NOTE: There was no keychain item to be deleted in tearDown.")
-//        }
-//
-//        // Remove local test directory
-//        removeDir(at: testsBasepath)
-//
-//        // Remove remote test directory
-//        do {
-//            try defaultTransferHandler?.remove(path: testsBasepath, isDir: true)
-//        } catch _ {
-//            print("NOTE: There was no remote test directory to be deleted in tearDown.")
-//        }
-//    }
+    override func setUp() {
+        // Create local test directory
+        createDir(at: testsBasepath)
+
+        // Create remote test directory
+        do {
+            let f = LocalConnection(path: testsBasepath)
+            let t = try SFTPConnection(path: SFTPServer.path, host: SFTPServer.host, port: SFTPServer.port, user: SFTPServer.user, authentication: .password(value: SFTPServer.password))
+            defaultTransferHandler = SFTPTransferHandler.init(from: f, to: t)
+            try defaultTransferHandler?.upload(path: testsBasepath, isDir: true)
+        } catch let error {
+            if error is KeychainError {
+                fatalError("Couldn't create SFTPConnection object.\nERROR: \(error)")
+            } else {
+                fatalError("Couldn't upload remote test directory at path \'\(SFTPServer.path)\'.\nERROR: \(error)")
+            }
+        }
+    }
+
+    override func tearDown() {
+        // Remove keychain item
+        do {
+            try KeychainGuard.removeItem(user: SFTPServer.user, server: SFTPServer.host)
+        } catch _ {
+            print("NOTE: There was no keychain item to be deleted in tearDown.")
+        }
+
+        // Remove local test directory
+        removeDir(at: testsBasepath)
+
+        // Remove remote test directory
+        do {
+            try defaultTransferHandler?.remove(path: testsBasepath, isDir: true)
+        } catch _ {
+            print("NOTE: There was no remote test directory to be deleted in tearDown.")
+        }
+    }
     
     // Tests if status is updated properly
     func testStatusUpdates() throws {
